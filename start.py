e8ce9cd01a68921f1adaddf91314d494f9c5cf75
RedEcho450 <alexander.vogt1000@web.de>
2022-09-09 14:34:05 +0200

    Loading Screen Simulator

--- start.py
+++ start.py
@@ -1,1 +1,18 @@
+import time
+import random
+import string
 print("Hello!")
+
+def randomGibberish()->str:
+    text = ""
+    for i in range(1, 40):
+        text += random.choice(string.hexdigits)
+    return text
+
+i = 0
+while(i < 100):
+    time.sleep(random.randint(10,50) * 0.01)
+    print(" " + str(i) + "% #" + randomGibberish() + "\r", end="")
+    i += random.randint(1,3)
+
+print("100%")    
